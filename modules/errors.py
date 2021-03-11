8db4c035f0b146bde75fbe5839af0d5abee574ab
Vincent Wu <vincentwu@ucsb.edu>
2021-03-10 20:35:15 -0800

    added custom errors

---
+++ modules/errors.py
@@ -0,0 +1,10 @@
+class MissingInputError(Exception):
+    """Exception raised for errors in the input.
+
+    Attributes:
+        expression -- input expression in which the error occurred
+        message -- explanation of the error
+    """
+
+    def __init__(self, message):
+        self.message = message
